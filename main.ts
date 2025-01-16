77d542656edeee296d06ecf146f16cea8a8d2beb
Alexander Michler <alexander@michler.io>
2025-01-16 09:04:11 +0100

    feat: keyboard feature

--- main.ts
+++ main.ts
@@ -1,85 +1,47 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {App, Modal, Plugin, Editor, MarkdownView, Notice, PluginSettingTab, Setting} from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface KbdPluginSettings {
+	shortcutFormat: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: KbdPluginSettings = {
+	shortcutFormat: '<kbd class="kbd">{shortcut}</kbd>'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class KbdPlugin extends Plugin {
+	settings: KbdPluginSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
+		// Add command to insert keyboard shortcut
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
+			id: 'insert-keyboard-shortcut',
+			name: 'Insert Keyboard Shortcut',
+			editorCallback: (editor: Editor) => {
+				const onSubmit = (shortcut: string) => {
+					if (shortcut) {
+						const formattedShortcut = this.settings.shortcutFormat.replace('{shortcut}', shortcut);
+						editor.replaceSelection(formattedShortcut);
 					}
+				};
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
+				// Create and show modal for shortcut input
+				new KbdInputModal(this.app, onSubmit).open();
 			}
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
+		// Register markdown processor to style shortcuts
+		this.registerMarkdownPostProcessor((element, context) => {
+			const kbdElements = element.querySelectorAll('.kbd');
+			kbdElements.forEach(el => {
+				// Add any additional styling or processing here if needed
+				el.addClass('kbd-plugin');
+			});
 		});
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
+		// Add settings tab
+		this.addSettingTab(new KbdSettingTab(this.app, this));
 	}
 
 	async loadSettings() {
@@ -91,43 +53,67 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
+class KbdInputModal extends Modal {
+	shortcut: string;
+	onSubmit: (shortcut: string) => void;
+
+	constructor(app: App, onSubmit: (shortcut: string) => void) {
 		super(app);
+		this.onSubmit = onSubmit;
 	}
 
 	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		const { contentEl } = this;
+
+		contentEl.createEl('h2', { text: 'Enter Keyboard Shortcut' });
+
+		new Setting(contentEl)
+			.setName('Shortcut')
+			.addText(text => text
+				.setPlaceholder('e.g., Ctrl+C')
+				.onChange(value => {
+					this.shortcut = value;
+				}));
+
+		new Setting(contentEl)
+			.addButton(btn => btn
+				.setButtonText('Insert')
+				.setCta()
+				.onClick(() => {
+					this.close();
+					this.onSubmit(this.shortcut);
+				}));
 	}
 
 	onClose() {
-		const {contentEl} = this;
+		const { contentEl } = this;
 		contentEl.empty();
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
+class KbdSettingTab extends PluginSettingTab {
+	plugin: KbdPlugin;
+
+	constructor(app: App, plugin: KbdPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
-
+		const { containerEl } = this;
 		containerEl.empty();
 
+		containerEl.createEl('h2', { text: 'Keyboard Shortcuts Settings' });
+
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Shortcut Format')
+			.setDesc('Format for inserting keyboard shortcuts. Use {shortcut} as placeholder.')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder(DEFAULT_SETTINGS.shortcutFormat)
+				.setValue(this.plugin.settings.shortcutFormat)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.shortcutFormat = value;
 					await this.plugin.saveSettings();
 				}));
 	}
